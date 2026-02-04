c983440585d482d5eb1f36a9d9dc0e4674945fbb
pavel <shh.pa@outlook.com>
2026-02-04 15:43:33 +0100

    register android map initializer

--- packages/google_maps_flutter/google_maps_flutter_android/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapActivity.java
+++ packages/google_maps_flutter/google_maps_flutter_android/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapActivity.java
@@ -1,7 +1,9 @@
 package io.flutter.plugins.googlemaps;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.gms.maps.MapView;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.nativeview.NativeView;
 import io.flutter.plugins.nativeview.NativeViewFlutterActivity;
 
@@ -9,6 +11,17 @@ import io.flutter.plugins.nativeview.NativeViewFlutterActivity;
 public class GoogleMapActivity extends NativeViewFlutterActivity {
 
   public static final String VIEW_KEY_MAP = "map";
+
+  @Nullable private GoogleMapInitializer googleMapInitializer;
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+
+    // Set up the MapsInitializerApi handler so Dart can call initializeWithRenderer() and warmup()
+    googleMapInitializer =
+        new GoogleMapInitializer(this, flutterEngine.getDartExecutor().getBinaryMessenger());
+  }
 
   @Override
   protected void onRegisterNativeViews() {
